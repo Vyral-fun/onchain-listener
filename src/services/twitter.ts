246a248804ee9b8f5694b80a0fb61984e8b7520e
tosynthegeek <oluwatosinojedapo@gmail.com>
2025-09-12 14:56:25 +0100

    feat: get twitter followers

---
+++ src/services/twitter.ts
@@ -0,0 +1,65 @@
+export async function getTwitterName(username: string) {
+  try {
+    const baseUrl = "https://api.twitterapi.io/twitter/user/search";
+
+    const url = new URL(baseUrl);
+    url.searchParams.append("query", username);
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "X-API-Key": Bun.env.TWITTER_API_KEY,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const name = data.users[0].name;
+    return name;
+  } catch (error) {
+    console.error("Error fetching Twitter details:", error);
+    return null;
+  }
+}
+
+export async function getTwitterFollowersName(username: string) {
+  const baseUrl = "https://api.twitterapi.io/twitter/user/followers";
+  const allFollowers: string[] = [];
+  let cursor = "";
+  let hasNextPage = true;
+  try {
+    while (hasNextPage) {
+      const url = new URL(baseUrl);
+      url.searchParams.append("userName", username);
+      url.searchParams.append("cursor", cursor);
+      url.searchParams.append("pageSize", "200");
+
+      const response = await fetch(url.toString(), {
+        method: "GET",
+        headers: {
+          "X-API-Key": Bun.env.TWITTER_API_KEY,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const names = (data.followers || []).map(
+        (user: { name: string }) => user.name
+      );
+      allFollowers.push(...names);
+      hasNextPage = data.has_next_page;
+      cursor = data.next_cursor;
+    }
+
+    return allFollowers;
+  } catch (error) {
+    console.error("Error fetching Twitter followers:", error);
+    return [];
+  }
+}
