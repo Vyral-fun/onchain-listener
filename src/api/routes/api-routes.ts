6a7840f460d1f3be4e019448db3c60ee088cfc65
tosynthegeek <oluwatosinojedapo@gmail.com>
2025-09-12 14:47:26 +0100

    feat: endpoint to add and get yapper referrals

--- src/api/routes/api-routes.ts
+++ src/api/routes/api-routes.ts
@@ -1,18 +1,26 @@
 import { Hono } from "hono";
 import {
   getAllJobs,
+  getJobClusters,
   getJobEventAddresses,
   getJobEvents,
   startContractEventListener,
-  stopContractEventListener,
 } from "../controllers/event-controllers";
+import {
+  addYapperReferral,
+  getYapperReferrals,
+} from "../controllers/yapper-controllers";
 
 const ApiRoutes = new Hono();
 
 ApiRoutes.post("/events/:jobId/start", startContractEventListener);
-ApiRoutes.post("/events/:jobId/stop", stopContractEventListener);
+// ApiRoutes.post("/events/:jobId/stop", stopContractEventListener);
 ApiRoutes.get("/:jobId/events", getJobEvents);
 ApiRoutes.get("/events/jobs", getAllJobs);
 ApiRoutes.get("/events/:jobId/addresses", getJobEventAddresses);
+ApiRoutes.get("/events/:jobId/clusters", getJobClusters);
+
+ApiRoutes.post("/referrals", addYapperReferral);
+ApiRoutes.get("/referrals/:yapperId", getYapperReferrals);
 
 export default ApiRoutes;
