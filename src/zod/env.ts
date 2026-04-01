f6b1d385b158591a2b5df31ad752dbcb7d78e882
tosynthegeek <oluwatosinojedapo@gmail.com>
2026-04-01 15:54:58 +0100

    add hedera config details to env.ts

--- src/zod/env.ts
+++ src/zod/env.ts
@@ -96,6 +96,15 @@ export const envSchema = z.object({
   MONAD_DEPOSIT_BACKUP_RPC_URL: z.string({
     required_error: "MONAD_DEPOSIT_BACKUP_RPC_URL is required",
   }),
+  HEDERA_PROVIDER_URL: z.string({
+    required_error: "HEDERA_PROVIDER_URL is required",
+  }),
+  HEDERA_DEPOSIT_PROVIDER_URL: z.string({
+    required_error: "HEDERA_DEPOSIT_PROVIDER_URL is required",
+  }),
+  HEDERA_DEPOSIT_BACKUP_PROVIDER_URL: z.string({
+    required_error: "HEDERA_DEPOSIT_BACKUP_PROVIDER_URL is required",
+  }),
 
   TWITTER_API_KEY: z.string({
     required_error: "TWITTER_API_KEY is required",
